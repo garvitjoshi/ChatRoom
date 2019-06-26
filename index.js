072a9ce32f4753463d35a4f0a10125dd3078d6de
Garvit Joshi <gajoshi@adobe.com>
2019-06-26 18:25:51 +0530

    chnage port no.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = 3001;
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
